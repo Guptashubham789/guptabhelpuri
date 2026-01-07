e19d1fdd4dc181f3fc1ff69ced368871e7803d16
Shubham <sg731159@gmail.com>
2026-01-08 00:29:46 +0530

    first commit

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+Platform  Firebase App Id
+web       1:792623897496:web:8b4e95ca4e5ea7ab4aa9ed
+android   1:792623897496:android:7b7661798af57b464aa9ed
+ios       1:792623897496:ios:27d977668e7ea6754aa9ed
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:guptabhelpuri/views/landing_screen.dart';
-
-void main() {
+import 'firebase_options.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Gupta Bhelpuri',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
